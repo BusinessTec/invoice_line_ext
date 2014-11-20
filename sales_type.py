41679b13faf476dc77559149caac49ea3db1907d
Business Tec Systems <BusinessTec@users.noreply.github.com>
2014-11-20 17:40:41 -0600

    Update and rename cost_plus_price.py to sales_type.py

---
+++ sales_type.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*- 
+
+from openerp import models, fields, api
+
+class sales_type(osv.osv):
+	_inherit = 'sale.order'
+    	_columns = {
+    	          	'sales_type': fields.selection([
+            ('wholesale', 'Distribuidores'),
+            ('consignment', 'Consignación'),
+            ('retail', 'Minoristas'),
+            ('spares', 'Repuestos'),      
+            ('other', 'Otro'),
+            ], 'Tipo de Ventas', select=True),
+    	}
+
