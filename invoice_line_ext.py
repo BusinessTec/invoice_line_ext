a9ed1fa17d5e08f99299c4c3e488e92334544b13
Business Tec Systems <BusinessTec@users.noreply.github.com>
2014-12-17 10:46:38 -0600

    Update invoice_line_ext.py

--- invoice_line_ext.py
+++ invoice_line_ext.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*- 
+from openerp import models, fields, api
 
-from openerp.osv import osv,fields,orm
+class invoice_line(models.Model):
+        _inherit = 'account.invoice.line'
+        period_id_ref = fields.Many2one('account.period', string='Periodo',compute="_periodo",readonly=True, store=True)
 
-class sales_type(osv.osv):
-	_inherit = 'account.invoice.line'
-	period_id_ref = fields.many2one('account.period', string='Periodo',compute="_periodo",readonly=True)
-	
-	@api.one
-	@api.depends('invoice_id.period_id')
-	def _periodo(self):
-		self.period_id_ref= self.invoice_id.period_id
 
+        @api.one
+        @api.depends('invoice_id.period_id')
+        def _periodo(self):
+                self.period_id_ref= self.invoice_id.period_id
